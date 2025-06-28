eec6bb659ec1bb0b8a0899ba4c369f54e4291347
Mide001 <olamideisreal5@gmail.com>
2025-06-28 23:42:53 +0100

    feat: configure Next.js for Google Drive image domains and optimization

--- next.config.js
+++ next.config.js
@@ -1,7 +1,53 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['localhost'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'drive.google.com',
+        pathname: '/uc/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'via.placeholder.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'imgur.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.imgur.com',
+        pathname: '/**',
+      },
+    ],
+    domains: [
+      'localhost',
+      'drive.google.com',
+      'lh3.googleusercontent.com',
+      'images.unsplash.com',
+      'picsum.photos',
+      'via.placeholder.com',
+      'imgur.com',
+      'i.imgur.com'
+    ],
     formats: ['image/webp', 'image/avif'],
   },
   experimental: {
